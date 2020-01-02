a39762aae673b936a166003bce6a6b9350ee268b
xuan-vy nguyen <xuanvy99@gmail.com>
2020-01-02 23:24:32 +0700

    Update server to Virginia-US
    same region with heroku server

--- mongodb.go
+++ mongodb.go
@@ -12,7 +12,7 @@ import (
 	"github.com/xuan-vy-nguyen/SE_Project01/database"
 )
 
-var MongoURI = "mongodb+srv://xuanvy99:az1731999@cluster0-mzeio.mongodb.net/test?retryWrites=true&w=majority" // "mongodb://localhost:27017"
+var MongoURI = "mongodb+srv://xuanvy:az1731999@cluster0-ktqay.mongodb.net/test?retryWrites=true&w=majority"// "mongodb+srv://xuanvy99:az1731999@cluster0-mzeio.mongodb.net/test?retryWrites=true&w=majority" // "mongodb://localhost:27017"
 var Collection = "app"
 var LoginDB = "LoginDB"
 var SignDB = "SignDB"
