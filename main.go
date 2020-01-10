80a62d165608d242522980ef62973f1a8feafac8
xuan-vy-nguyen <xuanvy99@gmail.com>
2020-01-10 13:26:05 +0700

    prepare for MobileDev

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var MongoURI = //"mongodb+srv://xuanvyClone1:az1731999@cluster0-ktqay.mongodb.net/test?retryWrites=true&w=majority"
-	"mongodb+srv://xuanvy99:az1731999@cluster0-mzeio.mongodb.net/test?retryWrites=true&w=majority" 
+var MongoURI = "mongodb+srv://xuanvyClone1:az1731999@cluster0-ktqay.mongodb.net/test?retryWrites=true&w=majority"
+	// "mongodb+srv://xuanvy99:az1731999@cluster0-mzeio.mongodb.net/test?retryWrites=true&w=majority" 
 	// "mongodb://localhost:27017"
 var Collection = "app"
 var LoginDB = "LoginDB"
